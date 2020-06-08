cc127e31ff777c48110fe3347f0a75f352499421
Anthony <cozziad@gmail.com>
2020-06-08 17:45:24 -0400

    Fix errors and navbar

--- stealth/settings/urls.py
+++ stealth/settings/urls.py
@@ -1,7 +1,7 @@
 from rest_framework import routers
 from .api import ContactInfoViewset
 
-router = routers.DefaultRouter
+router = routers.DefaultRouter()
 router.register('api/contactInfo', ContactInfoViewset, 'contactInfo')
 
-url_patterns = router.urls
+urlpatterns = router.urls
--- stealth/stealth/urls.py
+++ stealth/stealth/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('frontend.urls')),
     path('', include('settings.urls')),
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
 ]
